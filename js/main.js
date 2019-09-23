fa9f5e59ec89d0a40c2c14d32bf5b6ce32ea0ad7
nikolaskokhno <nikolaskokhno99@gmail.com>
2019-09-23 14:34:43 +0300

    update data user in ajax and update style table

--- js/main.js
+++ js/main.js
@@ -12,6 +12,18 @@ $(document).ready(function(){
 
     $(document).on('click', '#btn_add', function(){
 
+        var first_name = $('.first_name').val();
+        var second_name = $('.second_name').val();
+        var email = $('.email').val();
+
+        if(first_name.length < 1 || second_name < 1){
+            alert("Data can't be so short!");
+            return false;
+        }else if(email.length > 0 && (email.match(/.+?\@.+/g) || []).length !== 1){
+            alert('Email incorect!');
+            return false;
+        }
+
         $.ajax({
             url: 'insert.php',
             method: "POST",
@@ -25,8 +37,39 @@ $(document).ready(function(){
 
     });
 
+    function updateData(id, text, column_name){
+        $.ajax({
+            url: 'update.php',
+            method: 'POST',
+            data: {id:id, text:text, column_name:column_name},
+            dataType: 'text',
+            success: function(data){
+                alert(data);
+            }
+        });
+    }
+    
+    $(document).on('blur', '#first_name', function(){
+        var id = $(this).data("id1");
+        var first_name = $(this).text();
+        updateData(id, first_name, "first_name");
+    });
+
+    $(document).on('blur', '#second_name', function(){
+        var id = $(this).data("id2");
+        var second_name = $(this).text();
+        updateData(id, second_name, "second_name");
+    });
+
+    $(document).on('blur', '#email', function(){
+        var id = $(this).data("id3");
+        var email = $(this).text();
+        updateData(id, email, "email");
+    });
+
     $(document).on('click', '.btn_delete', function(){
         var id = $(this).data("id4");
+
         if(confirm("Are you sure do you want delete user data?")){
             $.ajax({
                 url: 'delete.php',
@@ -37,7 +80,6 @@ $(document).ready(function(){
                     alert(data);
                     fetch_data();
                 }
-
             });
         }   
     });
--- select.php
+++ select.php
@@ -6,13 +6,12 @@
     $result = mysqli_query($link, $sql);
 
     $outputUser .= '
-        <table class="table table-bordered user-table mt-3">
+        <table class="table table-bordered user-table mt-4">
             <thead class="thead-dark">
                 <tr>
                     <th scope="col">First Name</th>
                     <th scope="col">Second Name</th>
                     <th scope="col">E-mail</th>
-                    <th scope="col-sm-1">Update</th>
                     <th scope="col-sm-1">Delete</th>
                 </tr>
             </thead>
@@ -23,11 +22,10 @@
         while($row = mysqli_fetch_array($result)){
             $outputUser .= 
                 '<tr>
-                    <td class="first_name" data-id1="'.$row["id"].'">'.$row["first_name"].'</td>
-                    <td class="second_name" data-id2="'.$row["id"].'">'.$row["second_name"].'</td>
-                    <td class="email" data-id3="'.$row["id"].'">'.$row["email"].'</td>
-                    <td><button class="btn">&#10000;</button></td>
-                    <td><button class="btn btn_delete" data-id4="'.$row["id"].'">&#128465;</button></td>
+                    <td class="first_name" id="first_name" data-target="first_name" contenteditable="true" data-id1="'.$row["id"].'">'.$row["first_name"].'</td>
+                    <td class="second_name" id="second_name" data-target="second_name" contenteditable="true" data-id2="'.$row["id"].'">'.$row["second_name"].'</td>
+                    <td class="email" id="email" data-target="email" contenteditable="true" data-id3="'.$row["id"].'">'.$row["email"].'</td>
+                    <td><button class="btn btn_delete" data-id4="'.$row["id"].'"><i class="fas fa-trash"></i></button></td>
                 </tr>';
         }
 
